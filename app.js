2b4fcb25098edbc7204320f043f4ba313852b442
raulesgalindo <raulgalindorosales@gmail.com>
2018-04-13 18:32:52 -0600

    funcionan las rutas, se agrego contenedor de mongo y se agrego el archivo para docker composer

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ var logger = require('morgan');
 var path = require('path');
 var app = express();
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
@@ -20,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',indexRouter);
+app.use('/users',usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next();
@@ -35,5 +36,6 @@ res.locals.error = req.app.get('env') === 'development' ? err : {};
 res.status(err.status || 500);
 res.render('error');
 });
+
 
 module.exports = app;
