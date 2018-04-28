0ddb7e520c8268387f40eb842758865bb1976703
raulesgalindo <raulgalindorosales@gmail.com>
2018-04-27 18:35:22 -0600

    contenedor de python modificado, se agrego archivo json de fuentes de informacion, se cambio el mongo hacia el mongo existente del proyecto

--- python/api.py
+++ python/api.py
@@ -1,21 +1,24 @@
 import requests
 import pymongo
+import json
 from bson import json_util
 from pymongo import MongoClient
 
-
-#obtener data de API MinFin
-params = {
-    'sql': 'SELECT * FROM "105c966a-b71f-4db6-8e8e-caacca249823" limit 10', #Limite para no cargar todo
-}
-req = requests.get(
-    'https://datos.minfin.gob.gt/api/3/action/datastore_search_sql',
-    params=params)
-data = req.text
-#conectar a db mongo...
-client = MongoClient('localhost', 27017)
-db = client.presupuesto
-posts = db.posts
-data = json_util.loads(data)
-result = posts.insert_one(data)
-print(result)
+sources = json.load(open('sources.json'))
+for source in sources:
+    #obtener data de API MinFin
+    params = {
+        'sql': 'SELECT * FROM "'+source["codeSource"] +'" limit 10', 
+    }
+    req = requests.get(
+        source["apiUrl"],
+        params=params)
+    data = req.text
+    #conectar a db mongo...
+    client = MongoClient('mongo', 27017)
+    db = client[source["db"]]
+    collection = db[source["collection"]]
+    data = json_util.loads(data)
+    result = collection.insert_one(data)
+    print(result)
+exit
