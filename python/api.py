e526b8594f62d602b167f41a9b924c2509082460
raulesgalindo <raulgalindorosales@gmail.com>
2018-05-01 06:03:49 -0600

    insert batch para python listo, falta optimizarla

--- python/api.py
+++ python/api.py
@@ -5,20 +5,34 @@ from bson import json_util
 from pymongo import MongoClient
 
 sources = json.load(open('sources.json'))
+
 for source in sources:
+    client = MongoClient('mongo', 27017)
+    offset = 0
+    batchSize = int(source["batchSize"])
     #obtener data de API MinFin
     params = {
-        'sql': 'SELECT * FROM "'+source["codeSource"] +'" limit 10', 
+        'sql': 'SELECT COUNT(*) FROM "'+source["codeSource"] +'"'
     }
     req = requests.get(
         source["apiUrl"],
         params=params)
-    data = req.text
-    #conectar a db mongo...
-    client = MongoClient('mongo', 27017)
-    db = client[source["db"]]
-    collection = db[source["collection"]]
-    data = json_util.loads(data)
-    result = collection.insert_one(data)
-    print(result)
+    recordsCount = int(json.loads(req.content)["result"]["records"][0]["count"])
+    while offset < recordsCount:
+        params = {
+        'sql': 'SELECT * FROM "'+source["codeSource"] +'" OFFSET '+ str(offset) +' ROWS LIMIT '+ str(batchSize) 
+        }
+        req = requests.get(
+        source["apiUrl"],
+        params=params)
+        #conectar a db mongo...
+        db = client[source["db"]]
+        collection = db[source["collection"]]
+        documents = json.loads(req.content)["result"]["records"]
+        for document in documents:
+            filter = {'_id': document["_id"]}
+            result = collection.update(filter, document, upsert= True)
+            print(result)
+        offset += batchSize
+    client.close()
 exit
